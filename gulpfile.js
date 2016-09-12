a775475a4bff172de1b8ad8f3ccc3ff2edf8d914
Srinivasan <srinivasan.la10@gmail.com>
2016-09-12 15:11:29 +0530

    Template Started

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  webserver = require('gulp-webserver');
+
+gulp.task('js', function() {
+  gulp.src('builds/dev/js/**/*')
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/dev/*.html')
+});
+
+gulp.task('css', function() {
+  gulp.src('builds/dev/css/*.css')
+});
+
+gulp.task('watch', function() {
+  gulp.watch('builds/dev/js/**/*', ['js']);
+  gulp.watch('builds/dev/css/*.css', ['css']);
+  gulp.watch(['builds/dev/*.html',
+    'builds/dev/views/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('builds/dev/')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
